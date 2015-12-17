abc9eccc5c5967ea571ade5559046fbd3cb64029
spiffysparrow <tracykmullen@gmail.com>
2015-12-17 15:30:10 -0800

    refactored create under create new to make edit easier

--- frontend/components/poem.jsx
+++ frontend/components/poem.jsx
@@ -4,7 +4,6 @@ var ApiUtil = require('../util/apiUtil.js');
 module.exports = React.createClass({
 
   delete: function(e){
-    console.log("delete", e);
     ApiUtil.deletePoem(this.props.poem.id)
   },
 
@@ -18,22 +17,17 @@ module.exports = React.createClass({
     var selectedClass = "";
     highlightedText = passage.split("").map(function(ch, idx){
 
-      debugger
       if(isHighlighted(selects, idx)){
         selectedClass = "selected";
       }else{
         selectedClass = "";
       }
-      console.log(selectedClass);
-
       return <span key={idx} className={selectedClass} data-idx={idx}>{ch}</span>
     })
     return highlightedText;
   },
 
   render: function () {
-    // console.log(this.props.poem);
-
     var poem = this.props.poem
 
     pass = this.addHighlightSpans(poem.passage);
