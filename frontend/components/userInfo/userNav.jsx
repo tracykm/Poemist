5ee553e93149769b234ddcafd7fda01b7746b7f5
spiffysparrow <tracy@Tracys-MacBook-Pro.local>
2015-12-21 03:03:15 -0800

    poem zoom link

--- frontend/components/userInfo/userNav.jsx
+++ frontend/components/userInfo/userNav.jsx
@@ -11,11 +11,15 @@ module.exports = React.createClass({
     this.setState({ show_drop_down: !this.state.show_drop_down})
   },
 
+  _shutDropDown: function () {
+    this.setState({ show_drop_down: false})
+  },
+
   render: function () {
     return(
       <div className="userInfo userNav">
           <CurrentUserLink/> <span className="notifications" onClick={this._toggleDropDown}>></span>
-          { this.state.show_drop_down ? <DropDown shutDropDown={this._toggleDropDown} /> : null }
+          { this.state.show_drop_down ? <DropDown shutDropDown={this._shutDropDown} /> : null }
       </div>
     );
   }
