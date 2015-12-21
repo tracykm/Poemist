55849267f7a46fd4a7c8ce941fac3404462de408
spiffysparrow <tracy@Tracys-MBP.att.net>
2015-12-21 11:11:03 -0800

    fix get_poem_by_liker 500 error

--- frontend/components/indexViews/userProfile.jsx
+++ frontend/components/indexViews/userProfile.jsx
@@ -12,12 +12,20 @@ module.exports = React.createClass({
     this.history.pushState(null, url);
   },
   getInitialState: function () {
-    return { user: UserStore.find(this.props.user_id), poems: []};
+    var user = UserStore.find(this.props.user_id);
+    var poems = []
+    if(user){
+      poems = PoemStore.findPoems(user.poem_ids);
+    }
+    return { user: user, poems: poems};
   },
   componentDidMount: function () {
     this.userListener = UserStore.addListener(this._updateUser);
     this.poemListener = PoemStore.addListener(this._updatePoems);
     ApiUtil.getUser(this.props.user_id);
+  },
+  componentWillReceiveProps: function (nextProps) {
+    ApiUtil.getPoem(nextProps.params.poem_id);
   },
   componentWillUnmount: function () {
     this.userListener.remove();
