dc2d027e9b07c994a2a61bdfd74942dff01d9167
spiffysparrow <tracy@Tracys-MacBook-Pro.local>
2016-01-17 18:50:05 -0800

    normal cursor hover

--- frontend/components/singlePoem/poemTop.jsx
+++ frontend/components/singlePoem/poemTop.jsx
@@ -17,29 +17,18 @@ module.exports = React.createClass({
     this.history.pushState(null, "/edit/"+this.props.poem.id+"/create");
   },
 
-  goToPoem: function(e){
-    if(this.props.poem.id){
-      window.scrollTo(0,0);
-      this.history.pushState(null, "/poem/"+this.props.poem.id);
-    }
-  },
-
   render: function () {
     var deleteBtn = "";
     var editBtn = "";
-    var zoomBtn = "";
     if(window.current_user && parseInt(window.current_user.id) === this.props.poem.author_id){
       deleteBtn = <span className="deleteBtn" onClick={this.delete}>✕</span>;
       editBtn = <span className="editBtn" onClick={this.edit}>edit</span>;
-    }
-    if(!this.props.inCreateView){
-      zoomBtn = (<span className="zoomBtn" onClick={this.goToPoem}> <i className="icon-zoom-in"></i> </span>);
     }
 
 
     return(
       <div className="poemTopLeft">
-        {deleteBtn}{editBtn}{zoomBtn}
+        {deleteBtn}{editBtn}
       </div>
     );
 
