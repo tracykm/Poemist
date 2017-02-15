67a4265092bf4d9004230b0cad473f3b7b72bb5d
spiffysparrow <tracykmullen@gmail.com>
2017-02-14 21:04:48 -0800

    infinite scroll works with nums

--- src/containers/IndexView.jsx
+++ src/containers/IndexView.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import Poem from 'src/components/poem/Poem.jsx';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import './_indexView.scss';
 
-const IndexView = ({ poems }) => (
-  <div className="home-view">
-    {
-      poems ? poems.reverse().map((poem, i) => ( // make oldest to newest
-        poem && // TODO: remove this hack around null values when deleted
-          <Poem poem={poem} key={i} />
-      ))
-      : 'loading'
-    }
-  </div>
-);
+class IndexView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { list: [] }
+    this.loadFunc = this.loadFunc.bind(this);
+  }
+  loadFunc() {
+    this.state.list.push(this.state.list.length);
+    this.setState({ list: this.state.list });
+  }
+  render() {
+    return (
+      <div className="home-view">
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadFunc}
+          hasMore={true}
+          loader={<div className="loader">Loading ...</div>}
+          >
+          {this.state.list.map(item => <div key={item}>{item}</div>)}
+        </InfiniteScroll>
+      </div>
+    )
+  }
+}
 
 export default IndexView;
