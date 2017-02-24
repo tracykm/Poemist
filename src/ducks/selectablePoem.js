030205d4cf154b4b719c34298e8541aa3c1fd159
spiffysparrow <tracykmullen@gmail.com>
2017-02-23 22:45:16 -0800

    selectable poem duck working

--- src/ducks/selectablePoem.js
+++ src/ducks/selectablePoem.js
@@ -1,38 +1,40 @@
 import formatLetters from 'src/utils/formatLetters.js';
 import toggleLetters from 'src/utils/toggleLetters.js';
+import { from } from 'seamless-immutable';
 
-const initialState = {
+const initialState = from({
   isSelectingByWord: true,
   passage: null,
   wordLetters: [],
-};
+});
 
 module.exports = (state = initialState, action) => {
   switch (action.type) {
     case 'PASSAGE_RECEIVED': {
       const { title, id, text } = action.passage;
-      return {
-        ...state,
+      const attrs = {
         wordLetters: formatLetters({ passage: text }),
         passage: text,
         bookId: id,
         title,
       };
+      return state.merge(attrs);
     }
     case 'TOGGLE_SELECT_BY':
-      return { ...state, isSelectingByWord: !state.isSelectingByWord };
+      return state.set('isSelectingByWord', !state.get('isSelectingByWord'));
     case 'MAKE_POEM_SELECTABLE': {
       const { passage, selectedTexts } = action.poem;
-      return {
-        ...state,
+      const attrs = {
         passage,
         wordLetters: formatLetters({ passage, selectedTexts }),
       };
+      return state.merge(attrs);
     }
     case 'TOGGLE_SELECTED_LETTERS': {
       const { wordIdx, letterIdx } = action.letters;
       const { wordLetters, isSelectingByWord } = state;
-      return { ...state, wordLetters: toggleLetters({ wordLetters, wordIdx, letterIdx, isSelectingByWord }) };
+      const newWordLetters = toggleLetters({ wordLetters, wordIdx, letterIdx, isSelectingByWord });
+      return state.set('wordLetters', newWordLetters);
     }
     default:
       return state;
