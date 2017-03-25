e8d04bc1d4cb1bd88b80d4bf35a15dab3c61bb82
spiffysparrow <tracykmullen@gmail.com>
2017-03-24 17:20:37 -0700

    delete specs

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,16 @@ module Nambynonsense
       end
     end
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => false,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => false
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
