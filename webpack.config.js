74efd31c9acd625f1d29ec88699761ed88a956b6
Tracy <tmullen@clearblade.com>
2018-01-05 19:47:44 -0600

    change to preset

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,64 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var path = require('path')
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var webpackConfig = {
-  entry: './src/entry.jsx',
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "8888";
+
+loaders.push({
+  test: /\.scss$/,
+  loaders: ['style-loader', 'css-loader?importLoaders=1', 'sass-loader'],
+  exclude: ['node_modules']
+});
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.jsx', // your app's entry point
+  ],
+  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
   output: {
-    path: './',
-    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   module: {
-    loaders: [
-      { test: /\.js$|\.jsx$/, loaders: ['babel'], exclude: /node_modules/ },
-      {
-        test: /\.scss$|\.css$|\.sass$/,
-        loaders: ['style', 'css', 'sass'],
-      },
-      {
-        test: /\.png$|\.jpg$/,
-        loaders: ['url'],
-      },
-    ],
+    loaders
   },
-  devtool: 'source-maps',
-  resolve: {
-    root: path.resolve('./'),
-    extensions: ['', '.scss', '.css', '.js', '.json', '.jsx', '.png', '.jpg'],
+  devServer: {
+    contentBase: "./public",
+    // do not print bundle build stats
+    noInfo: true,
+    // enable HMR
+    hot: true,
+    // embed the webpack-dev-server runtime into the bundle
+    inline: true,
+    // serve index.html in place of 404 responses to allow HTML5 history
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true
     }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
-  node: {
-    fs: 'empty',
-  },
-}
-
-module.exports = webpackConfig
+    new DashboardPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      files: {
+        css: ['style.css'],
+        js: [ "bundle.js"],
+      }
+    }),
+  ]
+};
