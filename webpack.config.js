e697fc562600deea770de059cb0bb91ad24f0a64
Tracy <tmullen@clearblade.com>
2018-01-05 20:01:24 -0600

    good except proptypes

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-var webpack = require('webpack');
-var path = require('path');
-var loaders = require('./webpack.loaders');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var DashboardPlugin = require('webpack-dashboard/plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack')
+var path = require('path')
+var loaders = require('./webpack.loaders')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var DashboardPlugin = require('webpack-dashboard/plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "8888";
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || '8888'
 
 loaders.push({
   test: /\.scss$/,
   loaders: ['style-loader', 'css-loader?importLoaders=1', 'sass-loader'],
-  exclude: ['node_modules']
-});
+  exclude: ['node_modules'],
+})
 
 module.exports = {
   entry: [
     'react-hot-loader/patch',
-    './src/index.jsx', // your app's entry point
+    './src/entry.jsx', // your app's entry point
   ],
   devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
   output: {
     publicPath: '/',
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
-    loaders
+    loaders,
   },
   devServer: {
-    contentBase: "./public",
+    contentBase: './public',
     // do not print bundle build stats
     noInfo: true,
     // enable HMR
@@ -42,7 +42,7 @@ module.exports = {
     // serve index.html in place of 404 responses to allow HTML5 history
     historyApiFallback: true,
     port: PORT,
-    host: HOST
+    host: HOST,
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
@@ -50,15 +50,15 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin({
       filename: 'style.css',
-      allChunks: true
+      allChunks: true,
     }),
     new DashboardPlugin(),
     new HtmlWebpackPlugin({
       template: './src/template.html',
       files: {
         css: ['style.css'],
-        js: [ "bundle.js"],
-      }
+        js: ['bundle.js'],
+      },
     }),
-  ]
-};
+  ],
+}
