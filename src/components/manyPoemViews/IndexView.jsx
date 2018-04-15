f96c1e80bdf5bc1bad2e012c1566e54770d39790
Tracy <tmullen@clearblade.com>
2018-04-15 16:15:40 -0500

    user for all poem lists, but still glitchy on refetching last

--- src/components/manyPoemViews/IndexView.jsx
+++ src/components/manyPoemViews/IndexView.jsx
@@ -2,25 +2,23 @@ import PropTypes from 'prop-types';
 import React from 'react'
 import Poem from 'src/components/poem/Poem.jsx'
 import InfiniteScroll from 'react-infinite-scroller'
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import './_indexView.scss'
 
 class IndexView extends React.Component {
-  constructor() {
-    super()
-    this.loadFunc = this.loadFunc.bind(this)
-  }
-  loadFunc(page) {
-    this.props.getMorePoems(page - 1) // first page 0 for easier offset calls
-  }
   render() {
     const { poems, allPoemsLoaded } = this.props
     // console.log(poems.map(a => a.id))
     return (
       <div className="index-view">
         <InfiniteScroll
+          initialLoad={false}
+          threshold={0}
+          pageStart={0}
           className="poems-container"
-          loadMore={this.loadFunc}
+          loadMore={this.props.getMorePoems}
           hasMore={!allPoemsLoaded}
           loader={<div className="loader">Loading ...</div>}
         >
@@ -37,4 +35,74 @@ IndexView.propTypes = {
   allPoemsLoaded: PropTypes.bool,
 }
 
-export default IndexView
+
+
+const POEM_LIMIT = 4;
+
+const GetPoems = gql`
+  query GetPoems($offset: Int!, $authorId: Int) {
+    poems(limit: ${POEM_LIMIT}, offset: $offset, authorId: $authorId) {
+      id
+      styleId
+      backgroundId
+      colorRange
+      textChunks {
+        text
+        isSelected
+      }
+      author
+      authorId
+      createdAt
+      updatedAt
+    }
+  }
+`
+
+class IndexViewWData extends React.PureComponent {
+  state = {
+    allPoemsLoaded: false
+  }
+  getMorePoems = () => {
+    this.setState({ offset: this.state.offset + 10 })
+  }
+  render() {
+    return (
+      <Query
+        query={GetPoems}
+        variables={{
+          offset: 0,
+          authorId: this.props.userId
+        }}
+        notifyOnNetworkStatusChange
+        fetchPolicy="cache-and-network"
+      >
+        {({ loading, error, data, fetchMore }) => {
+          if (!data.poems) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+
+          return (
+            <IndexView
+              poems={data.poems}
+              allPoemsLoaded={this.state.allPoemsLoaded}
+              getMorePoems={(page) => {
+                const that = this
+                return fetchMore({
+                  variables: {
+                    offset: data.poems.length
+                  },
+                  updateQuery: (prev, { fetchMoreResult }) => {
+                    if (fetchMoreResult.poems.length < POEM_LIMIT) that.setState({ allPoemsLoaded: true })
+                    if (!fetchMoreResult) return prev;
+                    return Object.assign({}, prev, {
+                      poems: [...prev.poems, ...fetchMoreResult.poems]
+                    });
+                  }
+                })
+              }}
+            />)
+        }}
+      </Query>)
+  }
+}
+
+export default IndexViewWData
--- src/components/manyPoemViews/ProfileView.jsx
+++ src/components/manyPoemViews/ProfileView.jsx
@@ -55,141 +55,16 @@ const ProfileHeader = ({ user, current }) => {
   )
 }
 
-// const FEED_QUERY = gql`
-//   query Feed($type: FeedType!, $offset: Int, $limit: Int) {
-//     currentUser {
-//       login
-//     }
-//     feed(type: $type, offset: $offset, limit: $limit) {
-//       id
-//       # ...
-//     }
-//   }
-// `;
-
-const POEM_LIMIT = 4;
-
-const GetUserPoems = gql`
-  query GetUserPoems($offset: Int!, $authorId: Int) {
-    poems(limit: ${POEM_LIMIT}, offset: $offset, authorId: $authorId) {
-      id
-      styleId
-      backgroundId
-      colorRange
-      textChunks {
-        text
-        isSelected
-      }
-      author
-      authorId
-      createdAt
-      updatedAt
-    }
-  }
-`
-
-class UsersPoems extends React.PureComponent {
-  state = {
-    allPoemsLoaded: false
-  }
-  getMorePoems = () => {
-    this.setState({ offset: this.state.offset + 10 })
-  }
-  render() {
-    return (
-      <Query
-        query={GetUserPoems}
-        variables={{
-          offset: 0,
-          authorId: this.props.userId
-        }}
-        notifyOnNetworkStatusChange
-        fetchPolicy="cache-and-network"
-      >
-        {({ loading, error, data, fetchMore }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-
-          return (
-            <IndexView
-              poems={data.poems}
-              allPoemsLoaded={this.state.allPoemsLoaded}
-              getMorePoems={(page) => {
-                const that = this
-                return fetchMore({
-                  variables: {
-                    offset: data.poems.length
-                  },
-                  updateQuery: (prev, { fetchMoreResult }) => {
-                    if (fetchMoreResult.poems.length < POEM_LIMIT) that.setState({ allPoemsLoaded: true })
-                    if (!fetchMoreResult) return prev;
-                    return Object.assign({}, prev, {
-                      poems: [...prev.poems, ...fetchMoreResult.poems]
-                    });
-                  }
-                })
-              }}
-            />)
-        }}
-      </Query>)
-  }
-}
-
 class ProfileView extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (!prevProps.userId && this.props.userId) {
-      this.getMorePoems(0)
-    }
-  }
-  getMorePoems = (page) => {
-    const { userId, handleFetchUserPoems } = this.props
-    if (userId) {
-      handleFetchUserPoems({ userId, page })
-    }
-  }
   render() {
-    const { poems, user, userId, currentUserId, allPoemsLoaded } = this.props
+    const userId = Number(this.props.match.params.id)
     return (
       <div className="index-view">
         <ProfileHeaderWData id={userId} />
-        <UsersPoems userId={userId} />
+        <IndexView userId={userId} />
       </div>
     )
   }
 }
 
-ProfileView.propTypes = {
-  poems: PropTypes.array,
-  handleFetchUserPoems: PropTypes.func,
-  handleFetchUser: PropTypes.func,
-  allPoemsLoaded: PropTypes.bool,
-  userId: PropTypes.number,
-  currentUserId: PropTypes.number,
-  user: PropTypes.object,
-}
-
-const mapDispatchToProps = {
-  handleFetchUserPoems: poemDuck.handleFetchUserPoems,
-  handleFetchUser: userDuck.handleFetchUser,
-}
-
-function mapStateToProps(state, ownProps) {
-  const currentUserId = userDuck.getCurrentUserId(state)
-  const userId = Number(ownProps.match.params.id)
-  let allPoemsLoaded
-  let user
-  if (userId) {
-    // ugly beacuse any could be undefined
-    allPoemsLoaded = state.users[userId] && state.users[userId].allPoemsLoaded
-    user = userDuck.getUser(state, { userId })
-  }
-  return {
-    poems: _.values(poemDuck.getPoemsByUser(state, { userId })),
-    userId,
-    user,
-    currentUserId,
-    allPoemsLoaded,
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileView)
+export default ProfileView
