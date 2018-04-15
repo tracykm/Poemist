a9546eca7ab5a713b2389d7d27dfb48880071c5c
Tracy <tmullen@clearblade.com>
2018-04-15 14:33:47 -0500

    start trying to use apollo

--- src/entry.jsx
+++ src/entry.jsx
@@ -4,6 +4,28 @@ import { Provider } from 'react-redux'
 
 import store from 'src/store'
 import Routes from 'src/routes'
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: `${process.env.API_URL}/graphql`
+});
+
+import gql from "graphql-tag";
+
+client
+  .query({
+    query: gql`
+      {
+        user(id: 6) {
+          id
+          username
+          sessionToken
+          poemsWrittenCount
+        }
+      }
+    `
+  })
+  .then(result => console.log(result)).catch(e => console.log(e));
 
 ReactDOM.render(
   <Provider store={store}>
