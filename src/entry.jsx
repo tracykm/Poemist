40ee3cacdd6301699bccdf409202b0d2dadef783
Tracy <tmullen@clearblade.com>
2018-04-15 21:15:35 -0500

    has X-CSRF header

--- src/entry.jsx
+++ src/entry.jsx
@@ -4,22 +4,30 @@ import { Provider } from 'react-redux'
 
 import store from 'src/store'
 import Routes from 'src/routes'
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 const client = new ApolloClient({
-  uri: `${process.env.API_URL}/graphql`
-});
-
-import gql from "graphql-tag";
+  uri: `${process.env.API_URL}/graphql`,
+  fetchOptions: {
+    credentials: 'include',
+  },
+  request: operation => {
+    const token = localStorage.getItem('session')
+    operation.setContext({
+      headers: {
+        'X-CSRF-Token': token,
+      },
+    })
+  },
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Provider store={store}>
       <Routes />
     </Provider>
-  </ApolloProvider>
-  ,
+  </ApolloProvider>,
   document.getElementById('react'),
 )
 
