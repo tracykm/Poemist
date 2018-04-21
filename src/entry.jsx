eaf11d98affe9aaf4fa813b2c679ec0be2d4b5f1
Tracy <tmullen@clearblade.com>
2018-04-21 18:17:32 -0500

    Revert "try link state"
    This reverts commit 006d6255de5d3d2c8514b28e4cc71e5a3c646d73.

--- src/entry.jsx
+++ src/entry.jsx
@@ -7,10 +7,6 @@ import Routes from 'src/routes'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
-const defaultState = {
-  networkStatus: { __typename: 'NetworkStatus', isConnected: true },
-}
-
 const client = new ApolloClient({
   uri: `${process.env.API_URL}/graphql`,
   fetchOptions: {
@@ -23,24 +19,6 @@ const client = new ApolloClient({
         'X-CSRF-Token': token,
       },
     })
-  },
-  clientState: {
-    defaults: defaultState,
-    resolvers: {
-      Mutation: {
-        updateNetworkStatus: (_, { isConnected }, { cache }) => {
-          const data = {
-            networkStatus: {
-              __typename: 'NetworkStatus',
-              isConnected,
-            },
-          }
-          console.log('data', data)
-          cache.writeData({ data })
-          return null
-        },
-      },
-    },
   },
 })
 
